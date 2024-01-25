260de0f7a372f04294eb52a7c2da1661759878bf
Tushar <tsharma975@gmail.com>
2024-01-25 12:11:37 +0530

    first

---
+++ errors/custom-error.js
@@ -0,0 +1,8 @@
+class CustomAPIError extends Error {
+  constructor(message, statusCode) {
+    super(message)
+    
+  }
+}
+
+module.exports = CustomAPIError
---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const {UnauthenticatedError}=require('../errors')
+const jwt=require('jsonwebtoken')
+
+
+const authenticationMiddleware=async(req,res,next)=>{
+    
+    const authHeader=req.headers.authorization;
+    if(!authHeader||!authHeader.startsWith('Bearer ')){
+    throw new UnauthenticatedError('No token Provided')
+  }
+  const token=authHeader.split(' ')[1];
+  try {
+    const decoded=jwt.verify(token,process.env.JWT_SECRET)
+    const{id,username}=decoded;
+    req.user={id,username};
+    next();
+
+  } catch (error) {
+    throw new UnauthenticatedError('Not Authorizes to access this route')
+  }
+}
+module.exports=authenticationMiddleware
